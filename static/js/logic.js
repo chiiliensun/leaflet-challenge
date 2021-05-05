48e432ec8d7bb87fd028dd585908059e547db282
Chii-Lien Sun <chiilien.sun@gmail.com>
2021-05-04 17:59:28 -0700

    finished with basic code and starter

--- static/js/logic.js
+++ static/js/logic.js
@@ -26,7 +26,15 @@ const darkMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}
   accessToken: API_KEY
 });
 
-
+ // outdoor map
+ const darkMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+   tileSize: 512,
+   maxZoom: 18,
+   zoomOffset: -1,
+   id: "mapbox/outdoors-v11",
+   accessToken: API_KEY
+ });
 
 
 
